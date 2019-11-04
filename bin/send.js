87b23c35bfcb319d7251774b8f050d1e8a73831b
Woolson Lee <woolson.lee@gmail.com>
2019-11-04 15:04:28 +0800

    fix: 删除冗余代码,流程和文件

--- bin/send.js
+++ bin/send.js
@@ -3,23 +3,18 @@
 const program = require('commander')
 const chalk = require('chalk')
 const logger = require('../lib/logger')
-const CONFIG = require('../lib/aws-config')
+const CONFIG = require('../lib/config')
 const sendFile = require('../lib/send-file')
-const createConf = require('../lib/create-conf')
 
 program
   .version(require('../package').version)
   .usage(`<file|file-link> [options], ${chalk.bold('Run command config before send file!')}`)
-  .option('--hash', 'add hash at end of filename')
-  .option('-p, --path <path>', 'specific file storage path, default: it-platform')
   .option('-c, --clipboard', 'get image file from clip board')
-  .option('-n, --filename <name>', 'name the store file name')
   .parse(process.argv)
 
 if (program.args[0] !== 'config') {
   if (!Object.keys(CONFIG).length) {
     logger.info('No config be found, start config!')
-    createConf()
   } else {
     sendFile()
   }
