2e8fd8a46d6bb4a928cfc349bd049d313e0c6d11
Woolson Lee <woolson.lee@gmail.com>
2019-12-03 10:19:46 +0800

    docs: 更新文档链接和包名

--- bin/install.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs')
-const logger = require('../lib/logger')
-const { CONF_PATH } = require('../lib/config')
-
-fs.writeFileSync(CONF_PATH, 'IMAGUR_CLIENT_ID: 1dfa83c47f8a089')
-logger.info('Config file ~/.sendrc has been created')
