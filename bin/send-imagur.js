aa5e557ae8eec60433ed6c716896d1cafd1dfea6
Woolson Lee <woolson.lee@gmail.com>
2019-12-03 10:33:44 +0800

    fix: 修复错误单词写法

--- bin/send-imagur.js
+++ bin/send-imagur.js
@@ -9,9 +9,9 @@ program
   .option('-c, --clipboard', 'get image file from clipboard')
   .parse(process.argv)
 
-logger.info('Send By [Imagur]')
+logger.info('Send By [Imgur]')
 new Send({ // eslint-disable-line
   filePath: program.args[0],
   fromClipboard: program.clipboard || false,
-  imagur: true
+  imgur: true
 })
