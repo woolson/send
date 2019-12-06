a1ab95927982822ab0310307130d31ea3b351123
Woolson Lee <woolson.lee@gmail.com>
2019-12-06 15:30:16 +0800

    feat: 优化配置流程

--- bin/send-gitlab.js
+++ bin/send-gitlab.js
@@ -3,15 +3,25 @@
 const program = require('commander')
 const logger = require('../lib/logger')
 const Send = require('../lib/send')
+const { createConf, getConfig } = require('../lib/config')
 
 program
   .version(require('../package').version)
   .option('-c, --clipboard', 'get image file from clipboard')
+  .option('-e, --edit', 'edit config')
   .parse(process.argv)
 
-logger.info('Send By [GitLab]')
-new Send({ // eslint-disable-line
-  filePath: program.args[0],
-  fromClipboard: program.clipboard || false,
-  gitlab: true
-})
+if (program.edit) {
+  const config = getConfig()
+  createConf({
+    GITLAB_USER_NAME: config.GITLAB_USER_NAME,
+    GITLAB_USER_PASS: config.GITLAB_USER_PASS
+  }, 'gitlab')
+} else {
+  logger.info('Send By [GitLab]')
+  new Send({ // eslint-disable-line
+    filePath: program.args[0],
+    fromClipboard: program.clipboard || false,
+    gitlab: true
+  })
+}
--- bin/send-imgur.js
+++ bin/send-imgur.js
@@ -3,15 +3,24 @@
 const program = require('commander')
 const logger = require('../lib/logger')
 const Send = require('../lib/send')
+const { createConf, getConfig } = require('../lib/logger')
 
 program
   .version(require('../package').version)
   .option('-c, --clipboard', 'get image file from clipboard')
+  .option('-e, --edit', 'edit config')
   .parse(process.argv)
 
-logger.info('Send By [Imgur]')
-new Send({ // eslint-disable-line
-  filePath: program.args[0],
-  fromClipboard: program.clipboard || false,
-  imgur: true
-})
+if (program.edit) {
+  const config = getConfig()
+  createConf({
+    IMGUR_CLIENT_ID: config.IMGUR_CLIENT_ID
+  }, 'imgur')
+} else {
+  logger.info('Send By [Imgur]')
+  new Send({ // eslint-disable-line
+    filePath: program.args[0],
+    fromClipboard: program.clipboard || false,
+    imgur: true
+  })
+}
